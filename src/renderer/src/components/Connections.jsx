3b015a89a1708fb13adc539d606ac855f96be224
PSskal <ricpascual29@gmail.com>
2025-01-18 13:31:40 -0500

    add coneections+

--- src/renderer/src/components/Connections.jsx
+++ src/renderer/src/components/Connections.jsx
@@ -7,11 +7,11 @@ const Connections = () => {
   ]
 
   return (
-    <div className="w-40 mt-8 ml-16">
-      <div className="py-3 text-gray-600 uppercase border-b border-gray-600 text-xs">
+    <div className="w-40 mt-2 ml-2">
+      <div className="py-2 text-gray-600 uppercase border-b border-gray-600 text-xs">
         Connections
       </div>
-      <div className="pt-2 space-y-3">
+      <div className="pt-1 space-y-3">
         {connections.map((connection, index) => (
           <div key={index} className="flex justify-between text-xxxs">
             <span className="text-gray-600">{connection.name}</span>
